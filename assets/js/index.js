050e050ca22d5311de4f5da435d05b2fe9e29f8d
Phuclinh-coder <baclinhdzpro@gmail.com>
2022-10-26 11:36:27 +0700

    s2

--- assets/js/index.js
+++ assets/js/index.js
@@ -85,3 +85,11 @@ const BoxSingin = document.querySelector('.js-modal-box-signin')
         event.stopPropagation()
     })
 
+document.getElementById('next').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').appendChild(lists[0]);
+}
+document.getElementById('prev').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').prepend(lists[lists.length - 1]);
+}
